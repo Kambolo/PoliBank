cb0f10e9793757d3d038697c03532892d4343fa2
[K4mti] <kamil.boloz0407@gmail.com>
2023-11-17 15:52:28 +0100

    exchange

--- SignIn/src/main/java/com/example/signin/DesktopController.java
+++ SignIn/src/main/java/com/example/signin/DesktopController.java
@@ -1,14 +1,27 @@
 package com.example.signin;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class DesktopController {
+    @FXML
+    private BorderPane contentContainer;
+
+    public void exchangePanel(ActionEvent evt) throws IOException {
+        System.out.println("k");
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("exchange.fxml"));
+        contentContainer.setCenter(loader.load());
+    }
+
     @FXML
     public void initialize() throws IOException {
 
