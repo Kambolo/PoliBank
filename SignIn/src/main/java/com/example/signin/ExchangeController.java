1a6b3d062bbd2f5ced56a8490b2016c22935c54f
k4mti <kamil.boloz0407@gmail.com>
2023-12-05 13:21:22 +0100

    Doanie funkcjonalnosci wymiany walut

--- SignIn/src/main/java/com/example/signin/ExchangeController.java
+++ SignIn/src/main/java/com/example/signin/ExchangeController.java
@@ -1,23 +1,83 @@
 package com.example.signin;
 
+import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+
+import static com.example.signin.Main.getDbController;
 
 public class ExchangeController implements Initializable {
     @FXML
     private ChoiceBox<String> currencyID;
 
-    ObservableList<String> choices = FXCollections.observableArrayList("EUR","GBP", "USD");
+    private final ObservableList<String> choices = FXCollections.observableArrayList("EUR","GBP", "USD");
+
+    @FXML
+    private Button submit;
+    @FXML
+    private TextField exchangeValue;
+    @FXML
+    private Label afterExchangeValue;
+    Currencies currencies;
+    @FXML
+    private Label currency;
 
     @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
+    public void initialize(URL url, ResourceBundle resourceBundle){
+        currencies = new Currencies();
         currencyID.setValue(choices.get(1));
         currencyID.setItems(choices);
+
+        exchangeValue.textProperty().addListener((observable, oldValue, newValue)->{
+            try{
+                if (newValue != null && !newValue.isEmpty()) {
+                    double val = Double.parseDouble(newValue);
+                    double result = getResult(val);
+
+                    afterExchangeValue.setText(String.valueOf(result));
+                } else {
+                    // Handle empty or null input
+                    afterExchangeValue.setText("0");
+                }
+            }catch(NumberFormatException e){
+                afterExchangeValue.setText("Invalid Input");
+            }
+        });
+
+        currencyID.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue)->{
+            try{
+                currency.setText(newValue);
+            }catch(Exception e){
+                System.out.println("Error!");
+            }
+        });
     }
+
+    private double getResult(double val) {
+        double multiplier = switch (currencyID.getValue()) {
+            case "EUR" -> currencies.getEurSell();
+            case "GBP" -> currencies.getGbpSell();
+            case "USD" -> currencies.getUsdSell();
+            default -> 0.0d;
+        };
+        return val * multiplier;
+    }
+
+    public void clickedOnExchange(ActionEvent evt) throws ClassNotFoundException, SQLException {
+
+    }
+
 }
