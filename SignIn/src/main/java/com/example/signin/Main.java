a73eeeeed659e162bfe5696b376e94f446cb3b40
Kacper <kacperkiec11@gmail.com>
2023-12-13 13:38:59 +0100

    Implementacja wplat

--- SignIn/src/main/java/com/example/signin/Main.java
+++ SignIn/src/main/java/com/example/signin/Main.java
@@ -8,9 +8,12 @@ import java.io.IOException;
 
 public class Main extends Application {
     private static DbController dbController;
+    private static BankCustomer bankCustomer;
+
     @Override
     public void start(Stage stage) throws IOException {
         setDbController(new DbController());
+        setBankCustomer(new BankCustomer());
 
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("signIn.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
@@ -21,7 +24,8 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {launch();}
-
     public static DbController getDbController() {return dbController;}
     public void setDbController(DbController dbController) {Main.dbController = dbController;}
+    public static BankCustomer getBankCustomer() {return bankCustomer;}
+    public static void setBankCustomer(BankCustomer bankCustomer) {Main.bankCustomer = bankCustomer;}
 }
