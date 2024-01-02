dc095f77320934c7def68167af53123a19e14f8f
Kacper <kacperkiec11@gmail.com>
2024-01-02 19:47:54 +0100

    Dodanie stylu css

---
+++ SignIn/src/main/java/com/example/signin/AuthorsController.java
@@ -0,0 +1,38 @@
+package com.example.signin;
+
+import javafx.event.ActionEvent;
+
+import java.net.URI;
+import java.awt.Desktop;
+public class AuthorsController {
+
+    /**
+     * Metoda otwierajaca link do profilu na Github Kamila
+     * @param evt wcisniecie przycisku
+     */
+    public void kamilGithub(ActionEvent evt){
+        try {
+
+            Desktop d = Desktop.getDesktop();
+            d.browse(new URI("https://github.com/Kambolo"));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Metoda otwierajaca link do profilu na Github Kacpra
+     * @param evt wcisniecie przycisku
+     */
+    public void kacperGithub(ActionEvent evt){
+        try {
+
+            Desktop d = Desktop.getDesktop();
+            d.browse(new URI("https://github.com/KacperKiec"));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
--- SignIn/src/main/java/module-info.java
+++ SignIn/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.example.signin {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires java.desktop;
 
 
     opens com.example.signin to javafx.fxml;
